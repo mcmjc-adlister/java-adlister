7261cda195b8e8b31b96bc5a58f6b920b369b37d
Caroline E <caroline.esquivel01@gmail.com>
2020-11-16 11:58:39 -0600

    created tables

--- migration.sql
+++ migration.sql
@@ -2,6 +2,8 @@ USE adlister_db;
 
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS ad_categories_join_table;
 
 CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -16,7 +18,21 @@ CREATE TABLE ads (
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(240) NOT NULL,
     description TEXT NOT NULL,
+    time_stamp DATETIME,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
+);
+
+CREATE TABLE categories (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    category_name VARCHAR(45),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ad_categories_join_table (
+    categories_id INT UNSIGNED NOT NULL,
+    ads_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (categories_id) REFERENCES categories(id),
+    FOREIGN KEY (ads_id) REFERENCES ads(id)
 );
