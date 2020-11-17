4da1f86daa34df26f07ef0b48f9cde8e290008c4
Caroline E <caroline.esquivel01@gmail.com>
2020-11-17 13:33:11 -0600

    created UpdateInfoServlet and jsp for user to edit information

--- src/main/java/com/codeup/adlister/dao/Users.java
+++ src/main/java/com/codeup/adlister/dao/Users.java
@@ -2,10 +2,13 @@ package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface Users {
     User findByUsername(String username);
     Long insert(User user);
     User getUserByID(Long id);
+    boolean updateUser(User user) throws SQLException;
+    boolean deleteUser(int id) throws SQLException;
 }
