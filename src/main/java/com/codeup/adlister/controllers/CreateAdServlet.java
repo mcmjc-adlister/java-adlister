9a9cbf8ba6d1463f3be5e3ed684613b35e83f88f
Michael Meyer <michael.c.meyer26@gmail.com>
2020-11-19 11:31:21 -0600

    finished sticky forms on create ad pages

--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.Date;
@@ -20,21 +21,45 @@ public class CreateAdServlet extends HttpServlet {
             response.sendRedirect("/login");
             return;
         }
-        request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
-            .forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/ads/create.jsp").forward(request, response);
+
+        HttpSession session = request.getSession();
+        String errorMessage = (String) session.getAttribute("error");
+        if(errorMessage != null) {
+            request.setAttribute("error", errorMessage);
+            session.removeAttribute("error");//removes error message after session
+        }
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         User user = (User) request.getSession().getAttribute("user");
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
 
-        //TODO also insert category data into join table
-        Ad ad = new Ad(
-            user.getId(),
-            request.getParameter("title"),
-            request.getParameter("description"),
-            new Timestamp(new Date().getTime())
-        );
-        DaoFactory.getAdsDao().insert(ad);
-        response.sendRedirect("/ads");
+        //validate input
+        boolean inputHasErrors = title.isEmpty() || description.isEmpty();
+
+        if (inputHasErrors) {
+            if (title.isEmpty()) {
+                request.setAttribute("enteredDescription", description);
+                request.setAttribute("error", "Ad must have a title.");
+                request.getRequestDispatcher("/WEB-INF/ads/create.jsp").forward(request, response);
+            } else if (description.isEmpty()) {
+                request.setAttribute("enteredTitle", title);
+                request.setAttribute("error", "Ad must have a description.");
+                request.getRequestDispatcher("/WEB-INF/ads/create.jsp").forward(request, response);
+            }
+        } else {
+            //Create and save a new ad
+            //TODO also insert category data into join table
+            Ad ad = new Ad(
+                    user.getId(),
+                    title,
+                    description,
+                    new Timestamp(new Date().getTime())
+            );
+            DaoFactory.getAdsDao().insert(ad);
+            response.sendRedirect("/ads");
+        }
     }
 }
--- src/main/webapp/WEB-INF/ads/create.jsp
+++ src/main/webapp/WEB-INF/ads/create.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -8,14 +9,34 @@
 <body>
     <div class="container">
         <h1>Create a new Ad</h1>
+        <jsp:include page="/WEB-INF/partials/error.jsp"/>
         <form action="/ads/create" method="post">
             <div class="form-group">
                 <label for="title">Title</label>
-                <input id="title" name="title" class="form-control" type="text">
+                <%--Sticky forms check--%>
+                <c:choose>
+                    <%--If an enteredTitle value is passed in, fill it as the value for this input--%>
+                    <c:when test="${!(enteredTitle.equals(null))}">
+                        <input id="title" name="title" class="form-control" type="text" value="${enteredTitle}">
+                    </c:when>
+                    <%--Otherwise, as normal--%>
+                    <c:otherwise>
+                        <input id="title" name="title" class="form-control" type="text">
+                    </c:otherwise>
+                </c:choose>
             </div>
             <div class="form-group">
                 <label for="description">Description</label>
-                <textarea id="description" name="description" class="form-control" type="text"></textarea>
+                <c:choose>
+                    <%--If an enteredDescription value is passed in, fill it as the value for this input--%>
+                    <c:when test="${!(enteredDescription.equals(null))}">
+                        <textarea id="description" name="description" class="form-control" type="text">${enteredDescription}</textarea>
+                    </c:when>
+                    <%--Otherwise, as normal--%>
+                    <c:otherwise>
+                        <textarea id="description" name="description" class="form-control" type="text"></textarea>
+                    </c:otherwise>
+                </c:choose>
             </div>
             <input type="submit" class="btn btn-block btn-primary">
         </form>
